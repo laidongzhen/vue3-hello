88eb19af0595a7daf40768e0016737e237a5a2a2
laidongzhen <2685023408@qq.com>
2025-03-06 15:07:56 +0800

    feat:vue3基础语法编写，到018_watchEffect

---
+++ 备份/006_setup语法糖/components/Person.vue
@@ -0,0 +1,50 @@
+<!--
+ * @Author: laidz laidz@yelinked.com
+ * @Date: 2025-03-06 11:36:43
+ * @LastEditors: laidz laidz@yelinked.com
+ * @LastEditTime: 2025-03-06 14:19:27
+ * @Description:
+-->
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<script lang="ts" name="Person">
+  // 数据，原来是写在data中的，此时的name、age、tel都不是响应式的数据
+  let name= '张三'
+  let age= 18
+  let tel= '123456789'
+
+  // 修改姓名
+  function changeName() {
+    name = '李四' //注意：这样修改name，页面是没有变化的
+    console.log(name) //name确实改了，但name不是响应式的
+  }
+  // 修改年龄
+  function changeAge() {
+    age += 1//注意：这样修改age，页面是没有变化的
+    console.log(age) //age确实改了，但age不是响应式的
+  }
+  // 展示联系方式
+  function showTel(){
+    alert(tel)
+  }
+</script>
+
+<style scoped>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+button {
+  margin: 0 5px;
+}
+</style>
---
+++ 备份/013_watch监视_情况一/components/Person.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="person">
+    <h1>情况一：监视【ref】定义的【基本类型】的数据</h1>
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="changeSum">点我sum+1</button>
+  </div>
+</template>
+
+<script lang="ts" setup name="Person">
+import { ref, watch } from 'vue'
+// 数据
+let sum = ref(0)
+
+// 方法
+function changeSum() {
+  sum.value += 1
+}
+// 监视，情况一：监视【ref】定义的【基本类型】的数据
+const stopWatch = watch(sum, (newValue, oldValue) => {
+  console.log('newValue', newValue)
+  console.log('oldValue', oldValue)
+  if(newValue >= 10) {
+    stopWatch()
+  }
+})
+
+
+</script>
