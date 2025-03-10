8c50bc1ce9e02ef16968566f64b3c5ac8eb69f23
laidongzhen <2685023408@qq.com>
2025-03-10 14:40:41 +0800

    feat:vue3基础语法编写，到035_路由_重定向

--- 备份/031_路由_params参数/main.ts
+++ 备份/031_路由_params参数/main.ts
@@ -1,5 +1,5 @@
 // 引入createApp用于创建应用
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 // 引入App根组件
 import App from './App.vue'
 // 引入路由器
@@ -11,5 +11,3 @@ const app = createApp(App)
 app.use(router)
 // 挂载整个应用到app容器中
 app.mount('#app')
-
-// createApp(App).mount('#app')
