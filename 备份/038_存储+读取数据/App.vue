ea45a4defc1b8f487412b9594e4136a2f4f0a9a2
laidongzhen <2685023408@qq.com>
2025-03-14 14:13:54 +0800

    feat: vue3基础语法编写，到 6. 组件通信

---
+++ 备份/038_存储+读取数据/App.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: laidz laidz@yelinked.com
+ * @Date: 2025-03-14 11:17:41
+ * @LastEditors: laidz laidz@yelinked.com
+ * @LastEditTime: 2025-03-14 11:20:34
+ * @Description: 
+-->
+<template>
+  <Count/>
+  <br>
+  <LoveTalk/>
+  <!-- 页面中不能运行，代码没有写完整，在039可以查看效果 -->
+</template>
+
+<script setup lang="ts" name="App">
+  import Count from './components/Count.vue'
+  import LoveTalk from './components/LoveTalk.vue'
+</script>
